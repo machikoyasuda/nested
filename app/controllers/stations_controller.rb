44923786eaca8a7f91b4f3ead24352501d49df75
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-15 16:11:30 -0700

    Add instance variables to find line and station. Show them in the show page.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,7 +2,16 @@ class StationsController < ApplicationController
   def index
     # First, need to find the Line the Station belongs to
     @line = Line.find params[:line_id]
-    # Next, find the Stations via Mongoid method, .stations
+    # Next, find the all Stations in that Line via Mongoid method, .stations
     @stations = @line.stations
+  end
+
+  def show
+    # Show a specific station id
+    # GET stations#show /lines/:line_id/stations/:id line_station
+    @line = Line.find params[:line_id]
+    # Next, find the specific station in that Line
+    # .find(params[:id]) is a Mongoid method
+    @station = @line.stations.find(params[:id])
   end
 end
