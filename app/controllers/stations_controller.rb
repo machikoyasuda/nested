0ad00d04fd595930a8131a4be172f6be4cd6e911
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-15 15:07:42 -0700

    Add Station_Controller method to find correct stations embedded within a Line.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,4 +1,8 @@
 class StationsController < ApplicationController
   def index
+    # First, need to find the Line the Station belongs to
+    @line = Line.find params[:line_id]
+    # Next, find the Stations via Mongoid method, .stations
+    @stations = @line.stations
   end
 end
