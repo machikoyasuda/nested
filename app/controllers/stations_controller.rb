49ba4bfc11ad324dbc8600cce7114b715d1a8d47
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-15 16:23:37 -0700

    Add before_action hook to find which line the station is in.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,6 +1,9 @@
 class StationsController < ApplicationController
+  # HOOK: Parameter is method you want to call
+  before_action :line
+  # Every time index is hit, hit line method.
+
   def index
-    line
     # First, need to find the Line the Station belongs to
     # REFACTORED OUT: @line = Line.find params[:line_id]
     # Next, find the all Stations in that Line via Mongoid method, .stations
@@ -10,7 +13,6 @@ class StationsController < ApplicationController
   def show
     # Show a specific station id
     # GET stations#show /lines/:line_id/stations/:id line_station
-    line
     # Next, find the specific station in that Line
     # .find(params[:id]) is a Mongoid method
     @station = @line.stations.find(params[:id])
